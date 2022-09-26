c5889bdb2eb0a309c3dc321a006f7fe42a704997
Nymphium <s1311350@gmail.com>
2022-09-26 14:39:04 +0900

    Use `Lwt_process.exec` with timeout to terminate the process in time

--- client/test/test.ml
+++ client/test/test.ml
@@ -1,6 +1,6 @@
 module EchoService = Proto.Grpc_test.Echo
 
-let client = Grpc_client.make ~host:"loopback.ja-sore.de" ~port:50051 ()
+let client = Grpc_client.make ~host:"127.0.0.1" ~port:50051 ()
 let _unit = client.unary EchoService.unit' ()
 
 let greet () =
@@ -12,19 +12,19 @@ let () =
   Lwt_main.run
   @@
   let open Lwt.Syntax in
-  let* () =
-    let p = Lwt_process.(shell "yarn --offline node index.js" |> new process_none) in
-    Lwt_main.at_exit (fun () -> Lwt.return @@ p#kill 9);
-    Lwt_unix.sleep 2.
+  let p =
+    Lwt_process.(
+      shell "yarn --offline node index.js" |> exec ~stdout:`Dev_null ~timeout:4.)
   in
-  greet ()
-  |> Fun.flip Lwt.bind
-     @@ function
-     | Ok (msg, _) ->
-       print_endline msg;
-       Lwt.return_unit
-     | Error (err, msg, _tr) ->
-       let msg = Option.value msg ~default:"" in
-       Printf.eprintf "%s: %s\n" (Grpc_core.Status.Code.show_fail_bwd err) msg;
-       exit 1
+  let* () = Lwt_unix.sleep 1. in
+  let* res = greet () in
+  let* _ = p in
+  match res with
+  | Ok (msg, _) ->
+    print_endline msg;
+    Lwt.return_unit
+  | Error (err, msg, _tr) ->
+    let msg = Option.value msg ~default:"" in
+    Printf.eprintf "%s: %s\n" (Grpc_core.Status.Code.show_fail_bwd err) msg;
+    exit 1
 ;;
